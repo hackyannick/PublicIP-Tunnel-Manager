53c9e7512ffd0dfcae92138b2b322e8bbd210bd6
hackyannick <hack.yannick@gmail.com>
2023-10-06 10:19:16 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ tun_if="wg0"
 # Addresses  #
 ##############
 # Public IP used for tunnel. Should be the main IP of your VPS
-local_ip="88.99.224.189"
+local_ip="213.202.211.167"
 # Tunnel IP + network
 tun_local_addr="172.20.1.1/30"
 # Tunnel IP of your home router
@@ -33,14 +33,14 @@ tun_remote_addr6=""
 ###############
 # GRE: Should be 1476 for normal ethernet WAN and 1468 for PPPoE connections
 # WG: 1420 is default for Wireguard
-tun_mtu="1476"
+tun_mtu="1300"
 
 ################
 # Public IP(s) #
 ################
 # Define your public IPs that will be routed to your home router
 # Use an array ( "IP1/CIDR" "IP2 "IP3" ... )
-public_ip=( "95.217.241.212/32" )
+public_ip=( "213.202.208.195/32" )
 public_ip6=( )
 
 ################################
